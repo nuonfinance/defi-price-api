450ee367f450871dcd76b5d240d9bcc03bd2663d
koteld <festbot@gmail.com>
2025-02-22 20:43:25 +0100

    fix: add scaling factor for beefy seamless pool

--- calculations/beefyPool.js
+++ calculations/beefyPool.js
@@ -1,25 +1,30 @@
 const { ethers } = require('ethers');
 
-const { DEFAULT_SCALE_FACTOR_BN, beefyPoolAbi, tokenMapping } = require('../config');
+const { DEFAULT_SCALE_FACTOR_BN, beefyPoolAbi, tokenMapping, erc20Abi } = require('../config');
 const { fetchCoinGeckoPriceBN } = require('../services/coingecko');
 const { getFourPoolAssetPrice, isFourPool } = require('./fourPool');
 
 async function getBeefyPoolAssetPrice(poolAddress, provider) {
   const beefyPoolContract = new ethers.Contract(poolAddress, beefyPoolAbi, provider);
   const pricePerFullShareBN = await beefyPoolContract.getPricePerFullShare();
-  const underlyingAddress = await beefyPoolContract.want();
+  const underlyingAddress = (await beefyPoolContract.want()).toLowerCase();
   
   let underlyingPriceBN;
   if (await isFourPool(underlyingAddress, provider)) {
     underlyingPriceBN = await getFourPoolAssetPrice(underlyingAddress, provider);
   } else {
+    // TODO: Support more pool types
     // Otherwise, assume it's a regular ERC20
-    // Support more pool types
-    const coinId = tokenMapping[underlyingAddress.toLowerCase()];
+    const erc20Contract = new ethers.Contract(underlyingAddress, erc20Abi, provider);
+    const underlyingDecimals = await erc20Contract.decimals();
+    const vaultDecimals = await beefyPoolContract.decimals();
+    const scaleFactor = BigInt(10) ** BigInt(vaultDecimals - underlyingDecimals);
+    
+    const coinId = tokenMapping[underlyingAddress];
     if (!coinId) {
-      throw new Error(`Token mapping not found for coin at address ${underlyingAddress.toLowerCase()}`);
+      throw new Error(`Token mapping not found for coin at address ${underlyingAddress}`);
     }
-    underlyingPriceBN = await fetchCoinGeckoPriceBN(coinId);
+    underlyingPriceBN = (await fetchCoinGeckoPriceBN(coinId)) * scaleFactor;
   }
   
   return (pricePerFullShareBN * underlyingPriceBN) / DEFAULT_SCALE_FACTOR_BN;
